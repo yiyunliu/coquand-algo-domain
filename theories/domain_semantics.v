535f8d4acd015897561c022e48904eed06bb5082
Yiyun Liu <yliu8e5f7@protonmail.com>
2025-06-10 23:55:31 -0400

    Proving the transitivity of the algorithm (wip)

--- theories/domain_semantics.v
+++ theories/domain_semantics.v
@@ -46,6 +46,12 @@ with D_ne_eq (n : nat) : D_ne -> D_ne -> Prop :=
   D_eq n d0 d1 ->
   D_ne_eq n (D_App e0 d0) (D_App e1 d1).
 
+
+Scheme d_ne_eq_ind := Induction for D_ne_eq Sort Prop
+    with d_eq_ind := Induction for D_eq Sort Prop.
+Combined Scheme eq_mutual from d_ne_eq_ind, d_eq_ind.
+
+
 Definition S_Ne (d0 d1 : D) :=
   match d0, d1 with
   | D_Neu d0, D_Neu d1 => forall n, D_ne_eq n d0 d1
@@ -60,6 +66,38 @@ Fixpoint SEq (A : Ty) : D -> D -> Prop :=
   | Void => S_Ne
   | Fun A B => FunSpace (SEq A) (SEq B)
   end.
+
+Lemma D_eq_sym_mutual : forall n,
+  (forall a b, D_ne_eq n a b -> D_ne_eq n b a) /\
+    (forall a b, D_eq n a b -> D_eq n b a).
+Proof.
+  apply eq_mutual; qauto l:on ctrs:D_ne_eq, D_eq.
+Qed.
+
+Lemma D_eq_trans_mutual : forall n,
+  (forall a b, D_ne_eq n a b -> forall c, D_ne_eq n b c -> D_ne_eq n a c) /\
+    (forall a b, D_eq n a b -> forall c, D_eq n b c -> D_eq n a c).
+Proof.
+  apply eq_mutual.
+  - sfirstorder.
+  - hauto l:on ctrs:D_ne_eq inv:D_ne_eq.
+  - move => n e0 e1 he ihe u hu.
+    inversion hu; subst.
+    + constructor. eauto.
+    + econstructor; eauto.
+      inversion H1; subst.
+      constructor.
+
+
+
+Qed.
+
+Lemma SEq_Sym A d0 d1 : SEq A d0 d1 -> SEq A d1 d0.
+Proof.
+  elim : A d0 d1 => //=;
+                     ecrush unfold:S_Ne, FunSpace use:D_eq_sym_mutual.
+Qed.
+
 
 Lemma adequacy  : forall A,
     (forall n d0 d1, SEq A d0 d1 -> D_eq n d0 d1) /\
@@ -89,4 +127,58 @@ Proof.
       move => n.
       move : he => /(_ n). move /(ihA0 n) : hDom.
       clear. move => h0 h1. by constructor.
+Qed.
+
+Definition val_ok ρ0 ρ1 Γ :=
+  forall i A, lookup i Γ A -> SEq A (ρ0 i) (ρ1 i).
+
+Definition SemWt Γ a A := forall ρ0 ρ1, val_ok ρ0 ρ1 Γ -> exists d0 d1,
+      compile ρ0 a d0 /\ compile ρ1 a d1 /\ SEq A d0 d1.
+
+Lemma val_ok_ext ρ0 ρ1 Γ d0 d1 A :
+  SEq A d0 d1 ->
+  val_ok ρ0 ρ1 Γ ->
+  val_ok (d0.:ρ0) (d1.:ρ1) (A :: Γ).
+Proof.
+  move => h0 h1 n B.
+  inversion 1; subst.
+  - done.
+  - sfirstorder.
+Qed.
+
+
+Notation "Γ ⊨ a ∈ A" := (SemWt Γ a A) (at level 70).
+
+Lemma ST_Var Γ i A :
+  lookup i Γ A ->
+  Γ ⊨ VarTm i ∈ A.
+Proof.
+  hauto ctrs:compile unfold:val_ok.
+Qed.
+
+Lemma ST_Fun Γ b A B :
+  A :: Γ ⊨ b ∈ B ->
+  Γ ⊨ Abs b ∈ Fun A B.
+Proof.
+  move => h ρ0 ρ1 hρ.
+  do 2 eexists.
+  repeat split.
+  constructor. constructor.
+  simpl. move => d d' hd.
+  move : val_ok_ext hρ hd; repeat move/[apply].
+  move => {}/h.
+  hauto q:on ctrs:ap.
+Qed.
+
+Lemma ST_App Γ b a A B :
+  Γ ⊨ b ∈ Fun A B ->
+  Γ ⊨ a ∈ A ->
+  Γ ⊨ App b a ∈ B.
+Proof.
+  move => h0 h1 ρ0 ρ1 hρ.
+  move : h0 (hρ) => /[apply].
+  move => [b0][b1][hb0][hb1]hbe.
+  move : h1 (hρ) => /[apply].
+  move => [a0][a1][ha0][ha1]hae.
+  hauto lq:on unfold:FunSpace ctrs:compile.
 Qed.
--- theories/typing.v
+++ theories/typing.v
@@ -1,7 +1,7 @@
 Require Export imports.
 
 Reserved Notation "Γ ⊢ a ∈ A" (at level 70).
-
+Reserved Notation "Γ ⊢ a ≡ b ∈ A" (at level 70).
 Inductive Wt Γ : Tm -> Ty -> Prop :=
 | T_Var i A :
   lookup i Γ A ->
@@ -14,3 +14,28 @@ Inductive Wt Γ : Tm -> Ty -> Prop :=
   A::Γ ⊢ b ∈ B ->
   Γ ⊢ Abs b ∈ Fun A B
 where "Γ ⊢ a ∈ A" := (Wt Γ a A).
+
+Inductive Eq Γ : Tm -> Tm -> Ty -> Prop :=
+| J_Refl a A :
+  Γ ⊢ a ∈ A ->
+  Γ ⊢ a ≡ a ∈ A
+| J_Sym a b A :
+  Γ ⊢ a ≡ b ∈ A ->
+  Γ ⊢ b ≡ a ∈ A
+| J_Trans a b c A :
+  Γ ⊢ a ≡ b ∈ A ->
+  Γ ⊢ b ≡ c ∈ A ->
+  Γ ⊢ a ≡ c ∈ A
+| J_App b0 b1 a0 a1 A B :
+  Γ ⊢ b0 ≡ b1 ∈ Fun A B ->
+  Γ ⊢ a0 ≡ a1 ∈ A ->
+  Γ ⊢ App b0 a0 ≡ App b1 a1 ∈ B
+| J_β b a A B :
+  A :: Γ ⊢ b ∈ B ->
+  Γ ⊢ a ∈ A ->
+  Γ ⊢ App (Abs b) a ≡ b[a..] ∈ B
+| J_η a b A B :
+  A :: Γ ⊢ App a⟨shift⟩ (VarTm var_zero) ≡
+    App b⟨shift⟩ (VarTm var_zero) ∈ B ->
+  Γ ⊢ a ≡ b ∈ Fun A B
+where "Γ ⊢ a ≡ b ∈ A" := (Eq Γ a b A).
